da6b7b7fcd38811d66c0d25fb04fb3a6291e6a5a
Kyle Lennon <krlennon@mit.edu>
2022-03-04 17:11:34 -0500

    ignore dist directory

--- dist/_nuxt/73ba617.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{224:function(e,n,t){"use strict";t.r(n);var l={name:"IndexPage"},r=t(51),component=Object(r.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Swan Group Site")])}),[],!1,null,null,null);n.default=component.exports}}]);
--- dist/_nuxt/9b87fbe.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{171:function(n,o,w){w(172),n.exports=w(173)}},[[171,5,1,6]]]);
