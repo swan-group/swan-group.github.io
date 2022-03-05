5478bd84456df4c579a56da5137a814d8fe2b509
Kyle Lennon <krlennon@mit.edu>
2022-03-05 17:56:50 -0500

    add vue-typer to home page

---
+++ layouts/home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="body-content">
+    <!-- <section class="hero is-fullheight bg-img" id="header"> -->
+    <section class="hero bg-img" id="header">
+      <div class="hero-head">
+        <nav-bar></nav-bar>
+      </div>
+      <div class="hero-body">
+        <div class="typer-title">
+          <client-only>
+            <vue-typer
+              class="subtitle"
+              id="does-things"
+              :pre-type-delay="600"
+              :type-delay="70"
+              :pre-erase-delay="10000"
+	      erase-delay="70"
+              erase-style="backspace"
+              :text="[
+              'the swan group'
+              ]"
+            ></vue-typer>
+          </client-only>
+        </div>
+      </div>
+    </section>
+    <div id="main">
+      <nuxt/>
+    </div>
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+import NavBar from "~/components/NavBar.vue";
+import MyFooter from "~/components/MyFooter.vue";
+// if (process.browser) {
+//   require "~/plugins/particles."
+// }
+export default {
+  components: {
+    NavBar,
+    MyFooter,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -34,11 +34,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    "assets/css/main.scss",
+    '~/assets/css/main.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vue-typer.js', ssr: false },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
